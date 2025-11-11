b1184f7b3b2466b649be9d9ccb75f43c1fbe7034
devdavidias <davitdt206@gmail.com>
2025-11-10 21:37:08 -0300

    alteeração da linha do import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Header from "@/components/Header/Header";
-import Main from "@/components/Main/Main";
+
 import SobreMin from "@/components/SobreMin/SobreMin.js";
 import MenuHamburguer from "@/components/MenuHamberguer/MenuHamberguer";
 import Footer from "@/components/Footer/Footer.js";
